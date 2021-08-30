0684372003e5e6577013cfc2a225aafc4d1d8437
Andre McElroy jr <amcelroy50744@gmail.com>
2021-08-30 11:28:44 -0500

    intial commit

---
+++ QuoteGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KanyeWest
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine(kanyeQuote);
+        }
+        public static void RonSwanson()
+        {
+            var client = new HttpClient();
+            var ronSwanson = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronSwanson).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine(ronQuote);
+
+        }
+    }
+}
